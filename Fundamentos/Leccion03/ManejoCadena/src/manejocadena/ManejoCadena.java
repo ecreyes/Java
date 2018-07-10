492bb287380f1d20e3de924b6036338665c8c5cd
ecreyes <ereyesoyarzo@gmail.com>
2018-07-10 16:23:44 -0400

    Uso de String en Java

---
+++ Fundamentos/Leccion03/ManejoCadena/src/manejocadena/ManejoCadena.java
@@ -0,0 +1,17 @@
+package manejocadena;
+
+public class ManejoCadena {
+
+    public static void main(String[] args) {
+        String nombre = "Eduardo";
+        String apellido = "Reyes";
+        System.out.println("Concatenación: "+nombre+apellido);
+        System.out.println("Nueva linea: \n"+nombre+" "+apellido);
+        System.out.println("Tabulador: \t"+nombre+" "+apellido);
+        System.out.println("Retroceso: \b"+nombre+" "+apellido);
+        System.out.println("Retorno de carro: \r"+nombre+" "+apellido);
+        System.out.println("Comilla simple: \'"+nombre+" "+apellido+"\'");
+        System.out.println("Comilla doble: \""+nombre+" "+apellido+ "\"");
+    }
+    
+}
